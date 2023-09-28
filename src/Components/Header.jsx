1a8136ad7081088822c63650be5789290979624d
mukeshhr22 <msihag028@gmail.com>
2023-09-28 12:57:35 +0530

    finalSubmit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -11,17 +11,17 @@ import Sliders from '../Assets/Images/png/Sliders.png'
 const Header = () => {
     const settings = {
         dots: true,
-        arrows:false,
+        arrows: false,
         infinite: true,
         autoplay: false,
-        autoplaySpeed:2000,
+        autoplaySpeed: 2000,
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1
     };
     return (
         <>
-            
+
             <header className='bg_main position-relative min-vh-100 min_vh_75 overflow-hidden'>
                 <img className='position-absolute hero1 d-md-block d-none' src={Paper} alt="Paper" />
                 <img className='position-absolute hero2 d-md-block d-none' src={Paper} alt="Paper" />
@@ -32,17 +32,17 @@ const Header = () => {
                 <img className='position-absolute ball d-sm-block d-none' src={Ball} alt="Brick" />
                 <img className='position-absolute c d-sm-block d-none' src={C} alt="Brick" />
                 <img className='position-absolute tablet d-md-block d-none' src={Tablet} alt="Brick" />
-            
-            <CvNavbar/>
+
+                <CvNavbar />
                 <Container className='position-relative pt-md-5 pt-0 mt-md-4 mt-0'>
-                   
+
                     <div className='d-flex flex-column justify-content-center align-items-center pt-md-5 pt-2 ' data-aos="fade-right">
                         <h2 className='ff-primary fs-40 fc-black fw-600 text-center max-w-623 pt-5 mb-0'>
                             Unlock Your Potential with a Professional <span className='whitespace'>CV Builder</span>
                         </h2>
                         <p className='max-w-565 ff-primary fs-16 fw-400 fc-dark text-center mt-3'>Our user-friendly platform guides you through the process, allowing you to customize every section to align with your unique career goals.</p>
                     </div>
-                    <div className='d-flex flex_column justify-content-center align-items-center gap-3 mt-5' data-aos="fade-left">
+                    <div className='d-flex flex_column justify-content-center align-items-center gap-3 mt-5'>
                         <button className='ff-primary bg_primary fc-white fs-16 fw-600 headerbtn'>Get Started</button>
                         <button className='ff-primary bg_primary fc-white fs-16 fw-600 headerbtn'>Watch Demo</button>
                     </div>
@@ -61,7 +61,7 @@ const Header = () => {
                             <div className='d-flex justify-content-center align-items-center'>
                                 <img className='w-100' src={Sliders} alt="Paper" />
                             </div>
-                           
+
                         </Slider>
                     </div>
                 </Container>
